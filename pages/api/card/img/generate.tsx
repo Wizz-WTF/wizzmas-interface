75179fc698eead9bb6e217e941a5dc14fafffb34
sspzz <richiefilth@protonmail.com>
2022-12-17 00:36:35 +0100

    Use alchemy, use env to toggle mainnet/goerli

--- pages/api/card/img/generate.tsx
+++ pages/api/card/img/generate.tsx
@@ -1,23 +1,15 @@
 import { ethers } from 'ethers'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getProvider } from '../../../../contracts/Provider'
 import { card } from '../../../../lib/ImageUtil'
 import { getTemplateImagePath } from '../../../../lib/TemplateUtil'
-import { fetchERC721Artwork, fetchRunesWalkCycleFront } from '../../../../lib/TokenArtwork'
-
-function getProvider() {
-  return new ethers.providers.StaticJsonRpcProvider('http://127.0.0.1:8545', {
-    name: 'Anvil',
-    chainId: 31337,
-  })
-}
+import { fetchRunesWalkCycleFront } from '../../../../lib/TokenArtwork'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const template = parseInt(req.query.template as string, 10)
   const token = parseInt(req.query.token as string, 10)
   const tokenContract = req.query.contract as string
   const message = req.query.message as string
-
-  const provider = getProvider()
 
   var artworkImageURL = undefined
   if (req.query.template !== undefined) {
--- pages/api/template/img/[token].tsx
+++ pages/api/template/img/[token].tsx
@@ -1,15 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs'
 import path from 'path'
-import { ethers } from 'ethers'
 import { getCardsContract } from '../../../../contracts/WizzmasCardContract'
-
-function getProvider() {
-  return new ethers.providers.StaticJsonRpcProvider('http://127.0.0.1:8545', {
-    name: 'Anvil',
-    chainId: 31337,
-  })
-}
+import { getProvider } from '../../../../contracts/Provider'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.query.token == undefined) {

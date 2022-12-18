b2392fd9553d84fea553697463f652e72a5648c5
sspzz <richiefilth@protonmail.com>
2022-12-18 01:02:30 +0100

    Removing CSP

--- next.config.js
+++ next.config.js
@@ -13,11 +13,6 @@ const nextConfig = {
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; img-src 'self'; script-src 'self'; media-src 'self'",
-          }          
         ]
       }
     ]

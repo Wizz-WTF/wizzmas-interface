c17c5c10f3e6255fa6b8dd38cb67b9e91ab69034
0xJiro <jiro@sushi.com>
2022-12-20 01:10:24 -0500

    fixed mintEnabled bug

--- components/creator/Mint.tsx
+++ components/creator/Mint.tsx
@@ -88,7 +88,7 @@ const Mint: NextPage<MintProps> = ({ artworkType, templateType, message, token, 
       <p>This card will be sent to {recipient}</p>
       {numArtworks < 1 && <SmallTitle>You don't have any artworks!</SmallTitle>}
       <PrimaryButton disabled={!write || isLoading} onClick={() => write!()}>
-        {isLoading ? 'Minting...' : mintEnabled ? 'Mint Closed...' : 'Mint now'}
+        {isLoading ? 'Minting...' : !mintEnabled ? 'Mint Closed...' : 'Mint now'}
       </PrimaryButton>
       {(prepareError || error) && <DisplayError error={prepareError || error} />}
       {isSuccess && <SmallTitle>Congrats, you sent a Wizzmas Card to {recipient}!</SmallTitle>}
